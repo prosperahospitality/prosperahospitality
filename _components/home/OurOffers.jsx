c0f938fa165c92f91fe29c6917498778c63bf624
sameershaikh <sameer@prosperaahospitality.com>
2024-10-24 18:25:43 +0530

    updated code new

--- _components/home/OurOffers.jsx
+++ _components/home/OurOffers.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image'
 import IMAGES from '@/public/index'
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -11,91 +11,152 @@ import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const OurOffers = () => {
-    const [occupancyRate, setOccupancyRate] = useState(60);
-    const [repeatCustomerRate, setRepeatCustomerRate] = useState(80);
-    const [customerSatisfaction, setCustomerSatisfaction] = useState(60);
 
+    const [isBeginning, setIsBeginning] = useState(true);
+    const [isEnd, setIsEnd] = useState(false);
 
     const cardData = [
         {
-            alt: "diwali-festival",
-            name: "diwali-with-vinifera",
-            desktop: { src: IMAGES.festDiwali, width: 1600, height: 601, quality: 100 },
-            mobile: {
-                src: IMAGES.festDiwaliMob,
-                width: 750,
-                height: 1334,
-                quality: 100,
-            },
+          image: IMAGES.image1,
+          desc: (
+            <>
+              <div className="flex flex-col gap-1 mb-4 lg:mb-8">
+                <div className="text-sm md:text-md font-semibold text-[#800000]">
+                  WE HELP
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Hoteliers to Boost
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Profitability
+                </div>
+              </div>
+      
+              <div className="flex flex-col gap-1 mb-4 lg:mb-8">
+                <div className="text-sm md:text-md font-semibold text-[#800000]">
+                  THROUGH
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Data-driven Revenue
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Management
+                </div>
+              </div>
+            </>
+          ),
         },
         {
-            alt: "kedarnath-yatra",
-            name: "kedarnath yatra",
-            desktop: {
-                src: IMAGES.festKedarnath,
-                width: 1600,
-                height: 601,
-                quality: 100,
-            },
-            mobile: {
-                src: IMAGES.festKedarnathMob,
-                width: 750,
-                height: 1334,
-                quality: 100,
-            },
+          image: IMAGES.image2,
+          desc: (
+            <>
+              <div className="flex flex-col gap-1 mb-4 lg:mb-8">
+                <div className="text-sm md:text-md font-semibold text-[#800000]">
+                  WE HELP
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Hoteliers to
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Maximize Bookings
+                </div>
+              </div>
+      
+              <div className="flex flex-col gap-1 mb-4 lg:mb-8">
+                <div className="text-sm md:text-md font-semibold text-[#800000]">
+                  THROUGH
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Stunning websites
+                </div>
+              </div>
+            </>
+          ),
         },
-
         {
-            alt: "christmas-festival",
-            name: "christmas",
-            desktop: {
-                src: IMAGES.festChristmas,
-                width: 1600,
-                height: 601,
-                quality: 100,
-            },
-            mobile: {
-                src: IMAGES.festChristmasiMob,
-                width: 750,
-                height: 1334,
-                quality: 100,
-            },
-        },]
+          image: IMAGES.image3,
+          desc: (
+            <>
+              <div className="flex flex-col gap-1 mb-4 lg:mb-8">
+                <div className="text-sm md:text-md font-semibold text-[#800000]">
+                  WE HELP
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Hoteliers to
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Drive Revenue
+                </div>
+              </div>
+      
+              <div className="flex flex-col gap-1 mb-4 lg:mb-8">
+                <div className="text-sm md:text-md font-semibold text-[#800000]">
+                  THROUGH
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Targeted Digital
+                </div>
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-500">
+                  Marketing Campaigns
+                </div>
+              </div>
+            </>
+          ),
+        },
+    ];
 
     return (
         <div className="w-full flex justify-center items-center">
-            <div className="flex gap-0 md:gap-4 lg:gap-16 p-4 w-[90%] lg:[80%] lg:justify-center lg:items-center">
+            <div className="flex gap-4 lg:gap-8 p-4 w-[90%] lg:w-[80%] lg:justify-center lg:items-center">
                 <Swiper
-                    spaceBetween={30}
-                    pagination={{
-                        clickable: true,
+                    spaceBetween={20}
+                    navigation={{
+                        nextEl: ".swiper-next",
+                        prevEl: ".swiper-prev",
                     }}
-                    autoplay={true}
-                    navigation={true}
-                    modules={[Pagination, Navigation, Autoplay]}
-                    className="mySwiperGuest"
-                    style={{
-
-                        "--swiper-navigation-size": "20px",
-
+                    autoplay={{
+                        delay: 4000,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[Navigation, Autoplay]}
+                    onSlideChange={(swiper) => {
+                        setIsBeginning(swiper.isBeginning);
+                        setIsEnd(swiper.isEnd);
                     }}
                 >
-                    {cardData?.map((image, index) => (
+                    {cardData?.map((item, index) => (
                         <SwiperSlide key={index}>
+                            <div className='flex flex-col lg:flex-row gap-6 w-full justify-center items-center'>
+                                <div className='w-full lg:w-[50%]'>
+                                    <Image
+                                        src={item.image}
+                                        alt={`Image ${index + 1}`}
+                                        width={600}
+                                        height={600}
+                                        quality={100}
+                                        className='object-scale-down h-full w-full'
+                                    />
+                                </div>
+                                <div className='w-full lg:w-[50%] text-left lg:text-left'>
+                                    {item.desc}
 
-                            <div>
-
-                            </div>
-
-                            <div>
-                                <Image
-                                    src={IMAGES.image1}
-                                    objectFit='cover'
-                                    alt={index}
-                                    width={600}
-                                    height={600}
-                                    className='h-48 w-48'
-                                />
+                                    <div className="flex gap-4 justify-left lg:justify-start mt-8">
+                                        <button
+                                            className={`swiper-prev p-2 rounded-full bg-gray-200 text-gray-500 ${isBeginning ? "opacity-50" : ""
+                                                }`}
+                                            disabled={isBeginning}
+                                        >
+                                            <ChevronLeft className="w-6 h-6" />
+                                        </button>
+                                        <button
+                                            className={`swiper-next p-2 rounded-full bg-gray-200 text-gray-500 ${isEnd ? "opacity-50" : ""
+                                                }`}
+                                            disabled={isEnd}
+                                        >
+                                            <ChevronRight className="w-6 h-6" />
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </SwiperSlide>
                     ))}
