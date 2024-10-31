de85bd14cd54e5a2255c57bc15b2bb977d575cb9
AvinashProspera <avinash@prosperaahospitality.com>
2024-10-31 18:12:55 +0530

    last code from avi

--- _components/Prices/Prices.jsx
+++ _components/Prices/Prices.jsx
@@ -1,128 +1,84 @@
+"use client";
 import React from 'react';
-import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import { Check } from 'lucide-react';
 
 const plans = [
   {
-    name: 'Silver Plan',
+    name: 'Basic',
+    plan: (
+      <>
+        <div className='text-gray-600'>
+          <p className='text-sm mt-4 font-medium'>Everything you need for your Hotel Business.</p>
+        </div>
+        <div className='text-[#800000] flex flex-col gap-3 mt-7'>
+          <p className='text-sm line-through text-gray-500 font-semibold'> &#8377; <span>6,999/-</span> </p>
+          <p className='text-4xl font-bold'> &#8377; 4,999/-</p>
+        </div>
+        <div className='mt-8'>
+          <Button color="default" fullWidth radius='none' className='bg-[#800000] text-white font-semibold py-3'>
+            Choose Your Plan
+          </Button>
+        </div>
+      </>
+    ),
     services: [
-      {
-        category: 'Hotel Revenue Management',
-        items: [
-          'OTA Optimization',
-          'OTA Ranking Improvement',
-          'OTA Management',
-          'Inventory Management',
-          'Managing Rate Disparity',
-        ],
-      },
-      {
-        category: 'Hotel Social Media Marketing',
-        items: ['Social media marketing', 'Hotel Graphics'],
-      },
-      {
-        category: 'Hotel Reputation Management',
-        items: [
-          'Manage Review Site (Trip Advisor, Google Review, OTA Review)',
-          'Reply to guest reviews.',
-        ],
-      },
+      'Online Listing',
+      'Booking Management',
+      'Direct Check-Ins',
+      '24/7 Customer Support',
     ],
   },
   {
-    name: 'Gold Plan',
+    name: 'Standard',
+    plan: (
+      <>
+        <div className='text-gray-600'>
+          <p className='text-sm mt-4 font-medium'>Everything you need for your Hotel Business.</p>
+        </div>
+        <div className='text-[#800000] flex flex-col gap-3 mt-7'>
+          <p className='text-sm line-through text-gray-500 font-semibold'> &#8377; <span>25,000/-</span> </p>
+          <p className='text-4xl font-bold'> &#8377; 19,999/-</p>
+        </div>
+        <div className='mt-8'>
+          <Button color="default" fullWidth radius='none' className='bg-[#800000] text-white font-semibold py-3'>
+            Choose Your Plan
+          </Button>
+        </div>
+      </>
+    ),
     services: [
-      {
-        category: 'Hotel Revenue Management',
-        items: [
-          'Hotel Pricing',
-          'Managing Rate Disparity',
-          'Demand and supply pricing',
-          'Market Segment Pricing',
-          'Dynamic Pricing',
-          'OTA Optimization',
-          'OTA Ranking Improvement',
-          'OTA Management',
-          'Inventory Management',
-        ],
-      },
-      {
-        category: 'Hotel Digital Marketing',
-        items: [
-          'SEO friendly website',
-          'Website SEO',
-          'Social media marketing',
-          'Hotel Brand building and Awareness',
-          'Hotel Content Marketing',
-          'Retargeting Marketing',
-          'Hotel Photoshoot',
-          'Hotel Graphics',
-          'Lead generation',
-        ],
-      },
-      {
-        category: 'Hotel Reputation Management',
-        items: [
-          'Manage Review Site (Trip Advisor, Google Review, OTA Review)',
-          'Reply to guest reviews.',
-        ],
-      },
+      'All Basic Package Features',
+      'Enhanced Online Visibility',
+      'Monthly Performance Report',
+      'Custom Website Development',
+      '24/7 Customer Support'
     ],
   },
-
   {
-    name: 'Platinum Plan',
+    name: 'Premium',
+    plan: (
+      <>
+        <div className='text-gray-600'>
+          <p className='text-sm mt-4 font-medium'>Everything you need for your Hotel Business.</p>
+        </div>
+        <div className='text-[#800000] flex flex-col gap-3 mt-7'>
+          <p className='text-sm line-through text-gray-500 font-semibold'> &#8377; <span>45,000/-</span> </p>
+          <p className='text-4xl font-bold'> &#8377; 29,999/-</p>
+        </div>
+        <div className='mt-8'>
+          <Button color="default" fullWidth radius='none' className='bg-[#800000] text-white font-semibold py-3'>
+            Choose Your Plan
+          </Button>
+        </div>
+      </>
+    ),
     services: [
-      {
-        category: 'Hotel Revenue Management',
-        items: [
-          'Hotel Pricing',
-          'Managing Rate Disparity',
-          'Demand and supply pricing',
-          'Market Segment Pricing',
-          'Dynamic Pricing',
-          'OTA Optimization',
-          'OTA Ranking Improvement',
-          'OTA Management',
-          'Inventory Management',
-        ],
-      },
-      {
-        category: 'Hotel Digital Marketing',
-        items: [
-          'SEO friendly website',
-          'Website SEO',
-          'Social media marketing',
-          "Social media paid marketing",
-          "Hotel Google Ads",
-          'Hotel Brand building and Awareness',
-          'Hotel Content Marketing',
-          'Retargeting Marketing',
-          'Hotel Photoshoot',
-          'Hotel Graphics',
-          'Lead generation',
-        ],
-      },
-      {
-        category: 'Hotel Reputation Management',
-        items: [
-          'Manage Review Site (Trip Advisor, Google Review, OTA Review)',
-          'Reply to guest reviews.',
-        ],
-      },
-      {
-        category: 'B2B Marketing',
-        items: [
-          'Connecting to Travel agent',
-          'Generating bookings from B2b POS',
-        ],
-      },
-      {
-        category: 'Hotel Reservation Calls',
-        items: [
-          'Handing hotel calls for reservation in professional manner',
-          'Tracking the old leads and follow up with guest for higher conversion',
-        ],
-      },
+      'All Standard Package Features',
+      'Comprehensive Revenue Management',
+      'Digital Marketing Support',
+      'Monthly Strategy Report',
+      '24/7 Dedicated Customer Support'
     ],
   },
 ];
@@ -130,33 +86,40 @@ const plans = [
 const Prices = () => {
   return (
     <section className="w-[90%] lg:w-[80%] m-auto py-10">
-      <h1 className="text-4xl font-extralight text-center text-[#006FEE]">
-        Our Pricing
-      </h1>
-      <div className="my-10 grid gap-6 lg:flex w-full mx-auto">
+      <div className='flex flex-col gap-5 text-center'>
+        <h1 className="text-4xl font-bold text-[#800000]">
+          Choose your right plan.
+        </h1>
+        <p className='text-gray-600 text-lg'>Flexible solutions designed for your hotel’s success.</p>
+      </div>
+
+      <div className="my-10 flex lg:grid lg:grid-cols-3 w-full overflow-x-auto snap-x snap-mandatory scroll-smooth gap-5 lg:gap-6">
         {plans.map((plan, index) => (
-          <div
-            key={index}
-            className="w-full h-full border border-blue-500 rounded-xl p-4 hover:text-white hover:bg-blue-500"
+          <div 
+            key={index} 
+            className={`flex-shrink-0  border rounded-lg shadow-lg overflow-hidden snap-start ${plan.name === 'Standard' ? 'border-[#800000] relative' : 'border-gray-100'}`}
           >
-            <h2 className="mb-4 text-center text-2xl font-extralight">{plan.name}</h2>
-            {plan.services.map((service, idx) => (
-              <div key={idx}>
-                <p className="font-extralight ">{service.category}</p>
-                <ul className="list-disc pl-5 my-2">
-                  {service.items.map((item, i) => (
-                    <li key={i} className="text-base font-extralight">
-                      {item}
-                    </li>
-                  ))}
-                </ul>
+            {/* "Most Popular" banner for the Standard plan */}
+            {plan.name === 'Standard' && (
+              <div className='absolute top-0 left-0 w-full bg-[#800000] text-white text-center py-1 font-bold'>
+                Most Popular
               </div>
-            ))}
-            <Link href={`/contact-us`} passHref>
-              <div className="mt-6 inline-block px-4 py-2 border-white bg-blue-500 text-white shadow-2xl border rounded-full font-extralight tracking-wide hover:bg-blue-500 hover:text-white">
-                Read more
+            )}
+            <div className="p-6 mt-8">
+              <h2 className="text-2xl font-semibold text-[#800000] mb-2">{plan.name}</h2>
+              <div className='mb-6'>
+                {plan.plan}
               </div>
-            </Link>
+              <hr className='my-6' />
+              <ul className="flex flex-col gap-3 text-sm text-gray-600">
+                {plan.services.map((item, i) => (
+                  <li key={i} className="flex items-center gap-2">
+                    <Check className="text-[#0c8e00] size-5" />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         ))}
       </div>
